e175776c6457fe2d3b24d619a3c94700f10e626d
Sathprem Sital <23041625@student.hhs.nl>
2025-01-26 22:17:27 +0100

    make shit work with sql server

--- Project_WPR.Server/Program.cs
+++ Project_WPR.Server/Program.cs
@@ -1,20 +1,15 @@
+using Microsoft.EntityFrameworkCore;
 using Project_WPR.Server.data;
 using System.Text.Json.Serialization;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
-using Project_WPR.Server.Controllers;
 
-public class Program
-{
-    public static void Main(String[] args)
-    {
+public class Program {
+    public static void Main(String[] args) {
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-
-
         builder.Services.AddDbContext<DatabaseContext>(options =>
-            options.UseSqlite("Data Source=database.db"));
+            options.UseSqlServer("Server=database-carandall.clee44w6q7vn.eu-north-1.rds.amazonaws.com,1433;Database=database-carandall;User Id=admin;Password=AdminPassword123!;TrustServerCertificate=True;"));
+
         builder.Services.AddScoped<IDatabaseContext, DatabaseContext>();
         builder.Services.AddAuthorization();
         builder.Services.AddControllers()
@@ -23,13 +18,11 @@ public class Program
         builder.Services.AddIdentityApiEndpoints<User>()
             .AddEntityFrameworkStores<DatabaseContext>();
 
-        builder.Services.AddSwaggerGen(options =>
-        {
+        builder.Services.AddSwaggerGen(options => {
 
         });
 
-        builder.Services.AddCors(options =>
-        {
+        builder.Services.AddCors(options => {
             options.AddPolicy("Allowvite",
                builder => builder
                    .WithOrigins("https://localhost:50327")
@@ -42,46 +35,37 @@ public class Program
         builder.Services.AddLogging();
         builder.Services.AddHttpClient(); // For mail
 
-
         builder.Services.AddOpenApi();
 
         var app = builder.Build();
 
         // Apply pending migrations
-        using (var scope = app.Services.CreateScope())
-        {
+        using (var scope = app.Services.CreateScope()) {
             var dbContext = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
             dbContext.Database.Migrate();
         }
 
-            //app.UseDefaultFiles();
-            //app.MapStaticAssets();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment()) {
+            app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseCors("Allowvite");
-            app.UseAuthentication();
-            app.UseAuthorization();
-            app.MapControllers();
-            app.MapIdentityApi<User>();
-            app.MapFallbackToFile("/index.html");
-
+        app.UseHttpsRedirection();
+        app.UseCors("Allowvite");
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.MapIdentityApi<User>();
+        app.MapFallbackToFile("/index.html");
 
         // 1x runnen als de database leeg is
-        //using (var scope = app.Services.CreateScope())
-        //{
+        //using (var scope = app.Services.CreateScope()) {
         //    var dbc = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
         //    DataSeeder.Run(dbc);
         //}
 
         app.Run();
-
-        }
+    }
 }
